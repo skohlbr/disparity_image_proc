3b1515368220fc1efa9d8246b3140b719726a3a9
Stefan Kohlbrecher <kohlbrecher@sim.tu-darmstadt.de>
2014-05-19 12:48:12 +0200

    -Fix use of nonexistant include folder in CMakeLists.txt -Add license info

--- disparity_image_proc/src/nodelets/depth_image.cpp
+++ disparity_image_proc/src/nodelets/depth_image.cpp
@@ -1,36 +1,35 @@
-/*********************************************************************
-* Software License Agreement (BSD License)
-* 
-*  Copyright (c) 2008, Willow Garage, Inc.
-*  All rights reserved.
-* 
-*  Redistribution and use in source and binary forms, with or without
-*  modification, are permitted provided that the following conditions
-*  are met:
-* 
-*   * Redistributions of source code must retain the above copyright
-*     notice, this list of conditions and the following disclaimer.
-*   * Redistributions in binary form must reproduce the above
-*     copyright notice, this list of conditions and the following
-*     disclaimer in the documentation and/or other materials provided
-*     with the distribution.
-*   * Neither the name of the Willow Garage nor the names of its
-*     contributors may be used to endorse or promote products derived
-*     from this software without specific prior written permission.
-* 
-*  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
-*  "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
-*  LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
-*  FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
-*  COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
-*  INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
-*  BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
-*  LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
-*  CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
-*  LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
-*  ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
-*  POSSIBILITY OF SUCH DAMAGE.
-*********************************************************************/
+//
+// Software License Agreement (BSD License)
+//
+//=================================================================================================
+// Copyright (c) 2014, Stefan Kohlbrecher, TU Darmstadt
+// All rights reserved.
+
+// Redistribution and use in source and binary forms, with or without
+// modification, are permitted provided that the following conditions are met:
+//     * Redistributions of source code must retain the above copyright
+//       notice, this list of conditions and the following disclaimer.
+//     * Redistributions in binary form must reproduce the above copyright
+//       notice, this list of conditions and the following disclaimer in the
+//       documentation and/or other materials provided with the distribution.
+//     * Neither the name of the Simulation, Systems Optimization and Robotics
+//       group, TU Darmstadt nor the names of its contributors may be used to
+//       endorse or promote products derived from this software without
+//       specific prior written permission.
+
+// THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+// ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+// WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+// DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER BE LIABLE FOR ANY
+// DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+// (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+// LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+// ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+// (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+// SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+// Based on the point_cloud2 nodelet in the stereo_image_proc package
+
 #include <boost/version.hpp>
 #if ((BOOST_VERSION / 100) % 1000) >= 53
 #include <boost/thread/lock_guard.hpp>
@@ -196,138 +195,6 @@ void DepthImageNodelet::imageCb(const ImageConstPtr& l_image_msg,
 
 
   pub_depth_image_.publish(depth_image);
-
-  /*
-
-  // Fill in new PointCloud2 message (2D image-like layout)
-  PointCloud2Ptr points_msg = boost::make_shared<PointCloud2>();
-  points_msg->header = disp_msg->header;
-  points_msg->height = mat.rows;
-  points_msg->width  = mat.cols;
-  points_msg->fields.resize (4);
-  points_msg->fields[0].name = "x";
-  points_msg->fields[0].offset = 0;
-  points_msg->fields[0].count = 1;
-  points_msg->fields[0].datatype = PointField::FLOAT32;
-  points_msg->fields[1].name = "y";
-  points_msg->fields[1].offset = 4;
-  points_msg->fields[1].count = 1;
-  points_msg->fields[1].datatype = PointField::FLOAT32;
-  points_msg->fields[2].name = "z";
-  points_msg->fields[2].offset = 8;
-  points_msg->fields[2].count = 1;
-  points_msg->fields[2].datatype = PointField::FLOAT32;
-  points_msg->fields[3].name = "rgb";
-  points_msg->fields[3].offset = 12;
-  points_msg->fields[3].count = 1;
-  points_msg->fields[3].datatype = PointField::FLOAT32;
-  //points_msg->is_bigendian = false; ???
-  static const int STEP = 16;
-  points_msg->point_step = STEP;
-  points_msg->row_step = points_msg->point_step * points_msg->width;
-  points_msg->data.resize (points_msg->row_step * points_msg->height);
-  points_msg->is_dense = false; // there may be invalid points
- 
-  float bad_point = std::numeric_limits<float>::quiet_NaN ();
-  int offset = 0;
-  for (int v = 0; v < mat.rows; ++v)
-  {
-    for (int u = 0; u < mat.cols; ++u, offset += STEP)
-    {
-      if (isValidPoint(mat(v,u)))
-      {
-        // x,y,z,rgba
-        memcpy (&points_msg->data[offset + 0], &mat(v,u)[0], sizeof (float));
-        memcpy (&points_msg->data[offset + 4], &mat(v,u)[1], sizeof (float));
-        memcpy (&points_msg->data[offset + 8], &mat(v,u)[2], sizeof (float));
-      }
-      else
-      {
-        memcpy (&points_msg->data[offset + 0], &bad_point, sizeof (float));
-        memcpy (&points_msg->data[offset + 4], &bad_point, sizeof (float));
-        memcpy (&points_msg->data[offset + 8], &bad_point, sizeof (float));
-      }
-    }
-  }
-
-  // Fill in color
-  namespace enc = sensor_msgs::image_encodings;
-  const std::string& encoding = l_image_msg->encoding;
-  offset = 0;
-  if (encoding == enc::MONO8)
-  {
-    const cv::Mat_<uint8_t> color(l_image_msg->height, l_image_msg->width,
-                                  (uint8_t*)&l_image_msg->data[0],
-                                  l_image_msg->step);
-    for (int v = 0; v < mat.rows; ++v)
-    {
-      for (int u = 0; u < mat.cols; ++u, offset += STEP)
-      {
-        if (isValidPoint(mat(v,u)))
-        {
-          uint8_t g = color(v,u);
-          int32_t rgb = (g << 16) | (g << 8) | g;
-          memcpy (&points_msg->data[offset + 12], &rgb, sizeof (int32_t));
-        }
-        else
-        {
-          memcpy (&points_msg->data[offset + 12], &bad_point, sizeof (float));
-        }
-      }
-    }
-  }
-  else if (encoding == enc::RGB8)
-  {
-    const cv::Mat_<cv::Vec3b> color(l_image_msg->height, l_image_msg->width,
-                                    (cv::Vec3b*)&l_image_msg->data[0],
-                                    l_image_msg->step);
-    for (int v = 0; v < mat.rows; ++v)
-    {
-      for (int u = 0; u < mat.cols; ++u, offset += STEP)
-      {
-        if (isValidPoint(mat(v,u)))
-        {
-          const cv::Vec3b& rgb = color(v,u);
-          int32_t rgb_packed = (rgb[0] << 16) | (rgb[1] << 8) | rgb[2];
-          memcpy (&points_msg->data[offset + 12], &rgb_packed, sizeof (int32_t));
-        }
-        else
-        {
-          memcpy (&points_msg->data[offset + 12], &bad_point, sizeof (float));
-        }
-      }
-    }
-  }
-  else if (encoding == enc::BGR8)
-  {
-    const cv::Mat_<cv::Vec3b> color(l_image_msg->height, l_image_msg->width,
-                                    (cv::Vec3b*)&l_image_msg->data[0],
-                                    l_image_msg->step);
-    for (int v = 0; v < mat.rows; ++v)
-    {
-      for (int u = 0; u < mat.cols; ++u, offset += STEP)
-      {
-        if (isValidPoint(mat(v,u)))
-        {
-          const cv::Vec3b& bgr = color(v,u);
-          int32_t rgb_packed = (bgr[2] << 16) | (bgr[1] << 8) | bgr[0];
-          memcpy (&points_msg->data[offset + 12], &rgb_packed, sizeof (int32_t));
-        }
-        else
-        {
-          memcpy (&points_msg->data[offset + 12], &bad_point, sizeof (float));
-        }
-      }
-    }
-  }
-  else
-  {
-    NODELET_WARN_THROTTLE(30, "Could not fill color channel of the point cloud, "
-                          "unsupported encoding '%s'", encoding.c_str());
-  }
-
-  pub_points2_.publish(points_msg);
-  */
 }
 
 } // namespace stereo_image_proc
